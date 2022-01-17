8a73e23d62714581dfe1be13aa24b647b9ebb78b
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-01-17 22:19:43 +0800

    add valid parentheses func

--- HXHSwiftHub/Modules/AlgorithmVC/Stack/HXHStack.swift
+++ HXHSwiftHub/Modules/AlgorithmVC/Stack/HXHStack.swift
@@ -27,3 +27,70 @@ import Foundation
 /*
  可以用数组和链表实现
  */
+
+//MARK: - 数据结构定义
+class HXHStack {
+    private var list: [Character]
+    var size: Int {
+      return  list.count
+    }
+    var isEmpty: Bool {
+        return size == 0
+    }
+    func push(element: Character) {
+        list.append(element)
+    }
+    func pop() {
+        list.removeLast()
+    }
+    var top: Character? {
+        list.last
+    }
+    func clear() {
+        list.removeAll()
+    }
+    init() {
+        list = []
+    }
+}
+
+//MARK: - 栈的应用
+
+//1.浏览器的前进和后退.  类似的场景还有撤销，回复等。是一样的原理
+
+
+/*
+ 2. 有效的括号
+ 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+ 有效字符串需满足：
+
+ 左括号必须用相同类型的右括号闭合。
+ 左括号必须以正确的顺序闭合。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/valid-parentheses
+*/
+
+func validParentheses(s: String) -> Bool {
+    
+    var leftStack: HXHStack = HXHStack.init()
+    let _map:[Character:Character] = ["(":")",
+                               "[":"]",
+                               "{":"}"]
+    for c in s {
+        if _map.keys.contains(c) {
+            leftStack.push(element: c)
+        } else {
+            if let top = leftStack.top,
+               _map[top] == c {
+                print(leftStack.pop())
+            } else {
+                return false
+            }
+        }
+    }
+    
+    print(leftStack.size)
+    return leftStack.isEmpty
+}
