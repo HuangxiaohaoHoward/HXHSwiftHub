b8d2dc127b19657470e4e45d411c4ffd7e98ce6c
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-01-20 00:45:44 +0800

    add HXHQueue for queue concept

---
+++ HXHSwiftHub/Modules/AlgorithmVC/Queue/HXHQueue.swift
@@ -0,0 +1,22 @@
+//
+//  HXHQueue.swift
+//  HXHSwiftHub
+//
+//  Created by 黄小豪 on 2022/1/17.
+//
+
+import Foundation
+/*
+ 队列
+ 先进先出
+
+ 实现：
+ 可以用栈实现，入队(inStack)，出队(outStack)
+ 入队时，push到inStack中
+ 出队时
+ 
+ */
+
+class HXHQueue {
+    
+}
