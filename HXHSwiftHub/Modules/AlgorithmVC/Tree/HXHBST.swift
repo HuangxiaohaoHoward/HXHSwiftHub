7fe69779f35ef650ed0c3a0741c7c1764dfd6049
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-01-23 23:27:48 +0800

    添加队列和二叉搜索树的class

---
+++ HXHSwiftHub/Modules/AlgorithmVC/Tree/HXHBST.swift
@@ -0,0 +1,26 @@
+//
+//  HXHBST.swift
+//  HXHSwiftHub
+//
+//  Created by 黄小豪 on 2022/1/23.
+//
+
+import Foundation
+
+/*
+ 
+ 二叉搜索树 Binary Search Tree 简称BST
+
+ var size
+ var isEmpty
+ func clear
+ func add
+ func remove
+ func contain
+ 
+ 没有像数组那样的索引因为不适合，不准确
+*/
+
+class HXBinarySearchTree {
+    
+}
