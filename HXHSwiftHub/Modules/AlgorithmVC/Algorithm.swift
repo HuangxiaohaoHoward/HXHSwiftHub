d39a3865296622d14e36d1a19a7d94e12edff494
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-02-09 01:23:06 +0800

    optimize binary search func

--- HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
+++ HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
@@ -29,9 +29,46 @@ class Algorithm: NSObject {
 //        }
 //        return result
     //    }
+    //MARK: - 二分查找
+    /*
+     给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+     示例 1:
+
+     输入: nums = [-1,0,3,5,9,12], target = 9
+     输出: 4
+     解释: 9 出现在 nums 中并且下标为 4
+     示例 2:
+
+     输入: nums = [-1,0,3,5,9,12], target = 2
+     输出: -1
+     解释: 2 不存在 nums 中因此返回 -1
+      
+
+     提示：
+
+     你可以假设 nums 中的所有元素是不重复的。
+     n 将在 [1, 10000]之间。
+     nums 的每个元素都将在 [-9999, 9999]之间。
+
+     */
+    func binarySearch(_ nums: [Int], _ target: Int) -> Int {
+        var start = 0
+        var end = nums.count - 1
+        while start <= end {
+            let idx = (end - start ) / 2 + start
+            if target == nums[idx] {
+                return idx
+            } else if target > nums[idx] {
+                start = idx + 1
+            } else if target < nums[idx] {
+                end = idx - 1
+            }
+        }
+        return -1
+    }
     
-    /// 二分查找：判断有序的数组里面是否包含某一个数字
-    /// final
+    /// 二分法搜索 - 是否包含 （没啥用，只能返回是否包含，而且递归会引起栈溢出）
     func binarySearch(x: Int=3, nums: [Int]=[-1,0,3,5,9,12]) -> Bool {
         var result = false
         let min: Int = nums.startIndex
