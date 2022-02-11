f447da5e7628d1b57e144865952c0284138ab27a
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-02-11 23:32:54 +0800

    algorithm : 移动零

--- HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
+++ HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
@@ -453,4 +453,63 @@ class Algorithm: NSObject {
         }
         
     }
+    
+    //MARK: - 283. 移动零
+    /*
+     给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+     请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+
+      
+
+     示例 1:
+
+     输入: nums = [0,1,0,3,12]
+     输出: [1,3,12,0,0]
+     示例 2:
+
+     输入: nums = [0]
+     输出: [0]
+      
+
+     提示:
+
+     1 <= nums.length <= 104
+     -231 <= nums[i] <= 231 - 1
+      
+
+     进阶：你能尽量减少完成的操作次数吗？
+
+     */
+    // 两次都是看答案才写出来。说明对双指针不够熟练
+    func moveZeroes(_ nums: inout [Int]) {
+        var lastIndex: Int = 0
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums[lastIndex] = nums[i]
+                lastIndex += 1
+            }
+        }
+        while lastIndex < nums.count {
+            nums[lastIndex] = 0
+            lastIndex += 1
+        }
+        print(nums)
+    }
+    func moveZeroes2(_ nums: inout [Int]) {
+        var lastIndex: Int = 0
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums[lastIndex] = nums[i]
+                if i != lastIndex {
+                    nums[i] = 0
+                }
+                lastIndex += 1
+            }
+        }
+        print(nums)
+    }
+    
+    
+    
 }
