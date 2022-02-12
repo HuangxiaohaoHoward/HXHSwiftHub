0d340d863dd4bf0ffacc2578de70e24864e901dc
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2022-02-13 00:22:08 +0800

    algorithm : 反转字符串中的单词 III

--- HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
+++ HXHSwiftHub/Modules/AlgorithmVC/Algorithm.swift
@@ -410,7 +410,7 @@ class Algorithm: NSObject {
         print(3)
         print(nums)
     }
-
+    
     func reverse(nums: inout [Int], left: Int, right: Int) {
         
         var _left = left
@@ -428,29 +428,29 @@ class Algorithm: NSObject {
     //MARK: - 283. 移动零
     /*
      给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
-
+     
      请注意 ，必须在不复制数组的情况下原地对数组进行操作。
-
+     
       
-
+     
      示例 1:
-
+     
      输入: nums = [0,1,0,3,12]
      输出: [1,3,12,0,0]
      示例 2:
-
+     
      输入: nums = [0]
      输出: [0]
       
-
+     
      提示:
-
+     
      1 <= nums.length <= 104
      -231 <= nums[i] <= 231 - 1
       
-
+     
      进阶：你能尽量减少完成的操作次数吗？
-
+     
      */
     // 两次都是看答案才写出来。说明对双指针不够熟练
     func moveZeroes(_ nums: inout [Int]) {
@@ -482,38 +482,38 @@ class Algorithm: NSObject {
     }
     
     //MARK: - 167. 两数之和 II - 输入有序数组
-/*
-    给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
-
-    以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
-
-    你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
-
-    你所设计的解决方案必须只使用常量级的额外空间。
-
-    示例 1：
-
-    输入：numbers = [2,7,11,15], target = 9
-    输出：[1,2]
-    解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
-    示例 2：
-
-    输入：numbers = [2,3,4], target = 6
-    输出：[1,3]
-    解释：2 与 4 之和等于目标数 6 。因此 index1 = 1, index2 = 3 。返回 [1, 3] 。
-    示例 3：
-
-    输入：numbers = [-1,0], target = -1
-    输出：[1,2]
-    解释：-1 与 0 之和等于目标数 -1 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
-    提示：
-
-    2 <= numbers.length <= 3 * 104
-    -1000 <= numbers[i] <= 1000
-    numbers 按 非递减顺序 排列
-    -1000 <= target <= 1000
-    仅存在一个有效答案
- */
+    /*
+     给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+     
+     以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+     
+     你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+     
+     你所设计的解决方案必须只使用常量级的额外空间。
+     
+     示例 1：
+     
+     输入：numbers = [2,7,11,15], target = 9
+     输出：[1,2]
+     解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
+     示例 2：
+     
+     输入：numbers = [2,3,4], target = 6
+     输出：[1,3]
+     解释：2 与 4 之和等于目标数 6 。因此 index1 = 1, index2 = 3 。返回 [1, 3] 。
+     示例 3：
+     
+     输入：numbers = [-1,0], target = -1
+     输出：[1,2]
+     解释：-1 与 0 之和等于目标数 -1 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
+     提示：
+     
+     2 <= numbers.length <= 3 * 104
+     -1000 <= numbers[i] <= 1000
+     numbers 按 非递减顺序 排列
+     -1000 <= target <= 1000
+     仅存在一个有效答案
+     */
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
         // 双指针
         var left = 0
@@ -573,6 +573,40 @@ class Algorithm: NSObject {
             dict[num] = idx
         }
         return result
+    }
+    
+    //MARK: - 557. 反转字符串中的单词 III
+    /*
+     给定一个字符串 s ，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+     
+     示例 1：
+     
+     输入：s = "Let's take LeetCode contest"
+     输出："s'teL ekat edoCteeL tsetnoc"
+     示例 2:
+     
+     输入： s = "God Ding"
+     输出："doG gniD"
+     提示：
+     
+     1 <= s.length <= 5 * 104
+     s 包含可打印的 ASCII 字符。
+     s 不包含任何开头或结尾空格。
+     s 里 至少 有一个词。
+     s 中的所有单词都用一个空格隔开。
+     */
+    
+    func reverseWords(_ s: String) -> String {
+        let a = s.split(separator: " ")
+        var results: [String] = []
+        for str in a {
+            var _str = ""
+            str.forEach { c in
+                _str.insert(c, at: _str.startIndex)
+            }
+            results.append(_str)
+        }
+        return results.joined(separator: " ")
     }
     
 }
